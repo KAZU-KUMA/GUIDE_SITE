b472127419af102451259c12cadce2ecea6e9c7f
KAZUKI <qrki1ulf3d@gmail.com>
2020-11-04 21:56:29 +0900

    IE対応変更

--- wp_content/functions.php
+++ wp_content/functions.php
@@ -101,6 +101,9 @@ function pagination( $pages, $paged, $range = 2, $show_only = false ) {
 }
 
 // 定数
+//イベント説明文字数制限
 define("textLength", 77);
-define("eventCount", 6);
+//イベント一覧表示数
+define("eventCount", 3);
+//ニュース一覧表示数
 define("newsCount", 5);
--- wp_content/header.php
+++ wp_content/header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <title><?php bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="description" content=<?php bloginfo( 'description' ); ?>>
     <!-- ファビコン -->
     <link rel="icon" href="<?= get_template_directory_uri(); ?>/img/favicon.ico" type="image/png" sizes="16x16">
